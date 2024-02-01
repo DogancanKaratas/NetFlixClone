ee6fb155cb9f669badd3f4ac88798407f82c41af
DogancanKaratas <karatasdgncn21@gmail.com>
2024-02-01 22:01:04 +0300

    Add movie fetching functionality

---
+++ app/(private)/movies/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import VideoSection from './VideoSection'
+
+const HeroSection = () => {
+
+  return (
+    <div className='relative h-[50vw]'>
+        <VideoSection videoKey={videoKey} />
+
+    </div>
+  )
+}
+
+export default HeroSection
